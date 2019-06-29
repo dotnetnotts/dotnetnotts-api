a53d33e6f0ef31513adeef16dd4424d593d47679
Weiran Zhang <weiran@zhang.me.uk>
2019-06-29 14:26:09 +0100

    Added Azure KeyVault support for OAuth credentials

--- dotnetnotts-api/Program.cs
+++ dotnetnotts-api/Program.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 
 namespace dotnetnotts_api
 {
@@ -12,6 +16,19 @@ namespace dotnetnotts_api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            .ConfigureAppConfiguration((ctx, builder) =>
+            {
+                var keyVaultEndpoint = GetKeyVaultEndpoint();
+                var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                var keyVaultClient = new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(
+                        azureServiceTokenProvider.KeyVaultTokenCallback));
+                builder.AddAzureKeyVault(
+                    keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+
+            })
+            .UseStartup<Startup>();
+
+        private static string GetKeyVaultEndpoint() => "https://dotnetnotts-keyvault.vault.azure.net";
     }
 }
